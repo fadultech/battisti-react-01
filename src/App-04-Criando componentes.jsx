3e9a504d642e3955dac3b593b46fba128e9e8f19
Fadultech <jairo@fadul.tech>
2022-03-24 16:46:28 -0300

    04-Criando componentes

---
+++ src/App-04-Criando componentes.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import HelloWorld from './components/HelloWorld'  // importando o componente
+import Frase from './components/Frase'  // reutilizando um componente
+
+function App() {
+
+    // Renderizando o componente App
+    return (
+        <div className="App">
+            {/* Usando um componente */}
+            <HelloWorld />
+
+            {/* Reutilizando um componente */}
+            <Frase />
+        </div>
+    );
+}
+
+export default App;
